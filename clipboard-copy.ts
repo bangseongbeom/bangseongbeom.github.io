ddb5ed3b7455629e38c3b944f9a7fa8c08f6a41f
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-10-07 02:26:35 +0900

    Refactor the clipboard copy button

--- clipboard-copy.ts
+++ clipboard-copy.ts
@@ -1,39 +1,29 @@
 import type { RunnableCode } from "./runnable-code.ts";
 
-customElements.define(
-  "clipboard-copy",
-  class extends HTMLElement {
-    connectedCallback() {
-      this.querySelector("button")?.addEventListener("click", this);
+document.querySelectorAll("button.clipboard-copy").forEach((button) =>
+  button.addEventListener("click", (event) => {
+    let data: string;
+
+    let pre = button.closest(".highlight")?.querySelector("pre");
+    if (pre) data = pre.textContent;
+    else {
+      let runnableCode = button.closest("runnable-code") as RunnableCode | null;
+      data = runnableCode!.view!.state.doc.toString();
     }
 
-    disconnectedCallback() {
-      this.querySelector("button")?.removeEventListener("click", this);
-    }
+    navigator.clipboard.writeText(data);
 
-    handleEvent() {
-      let data: string;
-      let pre = this.closest(".highlight")?.querySelector("pre");
-      if (pre) data = pre.textContent;
-      else {
-        let runnableCode = this.closest("runnable-code") as RunnableCode | null;
-        data = runnableCode!.view!.state.doc.toString();
-      }
-
-      navigator.clipboard.writeText(data);
-
-      let copyIcon = this.querySelector(
-        ".js-clipboard-copy-icon",
-      ) as SVGElement;
-      let checkIcon = this.querySelector(
-        ".js-clipboard-check-icon",
-      ) as SVGElement;
-      copyIcon.attributeStyleMap.set("display", "none");
-      checkIcon.attributeStyleMap.delete("display");
-      setTimeout(() => {
-        copyIcon.attributeStyleMap.delete("display");
-        checkIcon.attributeStyleMap.set("display", "none");
-      }, 2000);
-    }
-  },
+    let copyIcon = button.querySelector(
+      ".js-clipboard-copy-icon",
+    ) as SVGElement;
+    let checkIcon = button.querySelector(
+      ".js-clipboard-check-icon",
+    ) as SVGElement;
+    copyIcon.attributeStyleMap.set("display", "none");
+    checkIcon.attributeStyleMap.delete("display");
+    setTimeout(() => {
+      copyIcon.attributeStyleMap.delete("display");
+      checkIcon.attributeStyleMap.set("display", "none");
+    }, 2000);
+  }),
 );
