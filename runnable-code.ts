8225f0eb227c47641d65977038e8dba53b6ede12
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-10-09 05:13:49 +0900

    Modify console to display error and warning as DOM elements

--- runnable-code.ts
+++ runnable-code.ts
@@ -73,15 +73,21 @@ async function runCode(event: Event) {
   ) as RunnableCode;
   let doc = runnableCode.view!.state.doc;
 
-  let outputValue: string;
+  let messages: HTMLElement[] = [];
 
   if (runnableCode.flag == "javascript" || runnableCode.flag == "js") {
-    let messages: string[] = [];
     let originalConsole = console;
     console = {
       ...originalConsole,
       assert(condition?: boolean, ...data: any[]) {
-        if (!condition) messages.push(...data.map((a) => String(a)));
+        if (!condition) {
+          let message = document.createElement("div");
+          message.classList.add("error");
+          message.textContent =
+            "Assertion failed: " +
+            (data.length ? data.join(" ") : "console.assert");
+          messages.push(message);
+        }
         originalConsole.assert(condition, ...data);
       },
       clear() {
@@ -89,32 +95,119 @@ async function runCode(event: Event) {
         originalConsole.clear();
       },
       debug(...data: any[]) {
-        messages.push(...data.map((a) => String(a)));
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = data.join(" ");
+        messages.push(message);
         originalConsole.debug(...data);
       },
       error(...data: any[]) {
-        messages.push(...data.map((a) => String(a)));
+        let message = document.createElement("div");
+        message.classList.add("error");
+        message.textContent = data.join(" ");
+        messages.push(message);
         originalConsole.error(...data);
       },
       info(...data: any[]) {
-        messages.push(...data.map((a) => String(a)));
+        let message = document.createElement("div");
+        message.classList.add("info");
+        message.textContent = data.join(" ");
+        messages.push(message);
         originalConsole.info(...data);
       },
       log(...data: any[]) {
-        messages.push(...data.map((a) => String(a)));
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = data.join(" ");
+        messages.push(message);
         originalConsole.log(...data);
       },
+      table(tabularData?: any, properties?: string[]) {
+        let message = document.createElement("table");
+        message.append(
+          ...tabularData.map((row: any) => {
+            let tr = document.createElement("tr");
+            tr.append(
+              ...row.map((cell: any) => {
+                let td = document.createElement("td");
+                td.textContent = cell;
+                return td;
+              }),
+            );
+            return tr;
+          }),
+        );
+        messages.push(message);
+        originalConsole.table(tabularData, properties);
+      },
+      trace(...data: any[]) {
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = data.join(" ");
+        messages.push(message);
+        originalConsole.trace(...data);
+      },
+      warn(...data: any[]) {
+        let message = document.createElement("div");
+        message.classList.add("warn");
+        message.textContent = data.join(" ");
+        messages.push(message);
+        originalConsole.warn(...data);
+      },
+      dir(item?: any, options?: any) {
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = item;
+        messages.push(message);
+        originalConsole.dir(item, options);
+      },
+      dirxml(...data: any[]) {
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = data.join(" ");
+        messages.push(message);
+        originalConsole.dirxml(...data);
+      },
     };
-    eval(doc.toString());
+    try {
+      eval(doc.toString());
+    } catch (error) {
+      let message = document.createElement("div");
+      message.classList.add("error");
+      message.textContent = String(error);
+      messages.push(message);
+    }
     console = originalConsole;
-    outputValue = messages.join("\n");
   } else if (runnableCode.flag == "python" || runnableCode.flag == "py") {
     await import("pyodide");
     if (!pyodide) {
       // @ts-expect-error
       pyodide = await loadPyodide();
     }
-    outputValue = pyodide.runPython(doc.toString());
+    pyodide.setStdout({
+      batched(output) {
+        let message = document.createElement("div");
+        message.classList.add("log");
+        message.textContent = output;
+        messages.push(message);
+      },
+    });
+    pyodide.setStderr({
+      batched(output) {
+        let message = document.createElement("div");
+        message.classList.add("error");
+        message.textContent = output;
+        messages.push(message);
+      },
+    });
+    try {
+      pyodide.runPython(doc.toString());
+    } catch (error) {
+      let message = document.createElement("div");
+      message.classList.add("error");
+      message.textContent = String(error);
+      messages.push(message);
+    }
   } else throw new Error();
 
   let output: HTMLOutputElement | null = runnableCode.querySelector("output");
@@ -125,5 +218,5 @@ async function runCode(event: Event) {
     );
     output = runnableCode.querySelector("output")!;
   }
-  output.value = outputValue;
+  output.replaceChildren(...messages);
 }
