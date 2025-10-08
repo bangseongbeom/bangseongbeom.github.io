afcb8a61440797fd6f46c4618fe2cadf575708d6
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-10-08 13:41:42 +0900

    Modify the style

--- build.ts
+++ build.ts
@@ -400,11 +400,12 @@ await Promise.all(
                         d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"
                       ></path>
                     </svg>
-                    코드 복사</button
-                  >,
+                    코드 복사
+                  </button>
+                  후
                   <a href="https://dev.java/playground/" target="_blank"
                     >The Java Playground</a
-                  >
+                  >에 붙여넣어 실행
                 </p>`,
                 { html: true },
               );
