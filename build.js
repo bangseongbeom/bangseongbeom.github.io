9dcb118f241fbe07060f5d2ea20c253692063bea
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2025-12-10 14:15:04 +0900

    Add custom 404 page

--- build.js
+++ build.js
@@ -774,7 +774,31 @@ await Promise.all(
             <body>
               ${header}
               <main class="page-content" aria-label="Content">
-                <div class="wrapper">${content}</div>
+                <div class="wrapper">
+                  ${pathToFileURL(join(sep, relative(SRC_ROOT, src)))
+                    .pathname === "/404.md"
+                    ? /* HTML */ `<style type="text/css" media="screen">
+                          .container {
+                            margin: 10px auto;
+                            max-width: 600px;
+                            text-align: center;
+                          }
+                          h1 {
+                            margin: 30px 0;
+                            font-size: 4em;
+                            line-height: 1;
+                            letter-spacing: -1px;
+                          }
+                        </style>
+
+                        <div class="container">
+                          <h1>404</h1>
+
+                          <p><strong>Page not found :(</strong></p>
+                          <p>The requested page could not be found.</p>
+                        </div>`
+                    : content}
+                </div>
               </main>
               ${footer}
             </body>
