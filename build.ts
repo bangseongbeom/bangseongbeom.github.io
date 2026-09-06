a41aae57844d304fe966a32ef5fe137060bd67f4
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2026-09-06 16:34:46 +0900

    Move the search trigger into the nav

--- build.ts
+++ build.ts
@@ -446,6 +446,7 @@ function runnableCode(document: Document, messages: Messages) {
 
 function navItems({ pages }: { pages: { title?: string; url: string }[] }) {
   return /* HTML */ `<div class="nav-items">
+    <pagefind-modal-trigger class="nav-item"></pagefind-modal-trigger>
     ${pages
       .map((page) =>
         page.title
@@ -473,44 +474,35 @@ function header({
         >${escape(siteTitle)}</a
       >
 
-      ${
-        navPages.length === 0
-          ? ""
-          : /* HTML */ `<nav class="site-nav">
-              <input type="checkbox" id="nav-trigger" />
-              <label for="nav-trigger">
-                <span class="menu-icon">
-                  <svg
-                    class="menu-icon-open"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                  >
-                    <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                    <path
-                      d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
-                    />
-                  </svg>
-                  <svg
-                    class="menu-icon-close"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 384 512"
-                  >
-                    <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                    <path
-                      d="M55.1 73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L147.2 256 9.9 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192.5 301.3 329.9 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.8 256 375.1 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192.5 210.7 55.1 73.4z"
-                    />
-                  </svg>
-                </span>
-              </label>
+      <nav class="site-nav">
+        <input type="checkbox" id="nav-trigger" />
+        <label for="nav-trigger">
+          <span class="menu-icon">
+            <svg
+              class="menu-icon-open"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+              <path
+                d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+              />
+            </svg>
+            <svg
+              class="menu-icon-close"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+              <path
+                d="M55.1 73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L147.2 256 9.9 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192.5 301.3 329.9 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.8 256 375.1 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192.5 210.7 55.1 73.4z"
+              />
+            </svg>
+          </span>
+        </label>
 
-              ${navItems({ pages: navPages })}
-            </nav>`
-      }
-
-      <div class="site-search">
-        <pagefind-modal-trigger></pagefind-modal-trigger>
-        <pagefind-modal></pagefind-modal>
-      </div>
+        ${navItems({ pages: navPages })}
+      </nav>
     </div>
   </header>`;
 }
@@ -1401,6 +1393,7 @@ function base({
             },
           ],
         })}
+        <pagefind-modal></pagefind-modal>
       </body>
     </html>`;
 }
